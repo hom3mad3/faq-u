cea75e8d9dfbf897b2f878ecac087a1d7ec8c136
hom3mad3 <micaelarava@gmail.com>
2016-08-06 13:04:56 +0200

    create custom-scripts.js and enqueue it on functions.php

---
+++ js/custom-scripts.js
@@ -0,0 +1,10 @@
+jQuery(window).load(function() {
+
+// MASSONRY Without jquery
+var container = document.querySelector('#ms-container');
+var msnry = new Masonry( container, {
+  itemSelector: '.ms-item',
+  columnWidth: '.ms-item',
+});
+
+  });
